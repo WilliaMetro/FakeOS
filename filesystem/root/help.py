064c2b8edd9fe7fca576513d57d9c93d25aa4095
WilliaMetro <williametronext@gmail.com>
2026-03-04 21:15:16 +0700

    fixed the help.py, is easy to see

--- filesystem/root/help.py
+++ filesystem/root/help.py
@@ -1,7 +1,14 @@
 import os
 
-def app(args):
-    print("Available commands:")
-    for file in os.listdir():
-        if file.endswith(".py") and file != "launchup.py":
-            print(" -", file.replace(".py", ""))
+def app(program_args):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    current_file = os.path.basename(__file__)
+
+    py_files = [
+        os.path.splitext(file)[0]
+        for file in os.listdir(current_dir)
+        if file.endswith(".py") and file != current_file
+    ]
+
+    for name in sorted(py_files):
+        print(name)
